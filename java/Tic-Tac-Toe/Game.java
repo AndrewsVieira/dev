05466d7fee34310f1e5e5dc28e2612c1e9d83225
AndrewsVieira <andrews.a.vieira@gmail.com>
2020-11-03 17:00:18 -0300

    jogo-da-velha

--- java/Tic-Tac-Toe/Game.java
+++ java/Tic-Tac-Toe/Game.java
@@ -24,14 +24,30 @@ public class Game {
 
     @Override
     public String toString() {
-        return String.format("|\t%s\t|\t%s\t|\t%s\t|\n|\t%s\t|\t%s\t|\t%s\t|\n|\t%s\t|\t%s\t|\t%s\t|\n\n",
-                getTicTacToe(0), getTicTacToe(1), getTicTacToe(2), getTicTacToe(3), getTicTacToe(4), getTicTacToe(5),
-                getTicTacToe(6), getTicTacToe(7), getTicTacToe(8));
+        return String.format(   "|\t%s\t|\t%s\t|\t%s\t|\n|\t%s\t|\t%s\t|\t%s\t|\n|\t%s\t|\t%s\t|\t%s\t|\n\n",
+                                getTicTacToe(0), 
+                                getTicTacToe(1), 
+                                getTicTacToe(2), 
+                                getTicTacToe(3), 
+                                getTicTacToe(4), 
+                                getTicTacToe(5),
+                                getTicTacToe(6), 
+                                getTicTacToe(7), 
+                                getTicTacToe(8));
     }
 
     public boolean checkWinningSequences(String symbol) {
-        int[][] winningSequences = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 },
-                { 0, 4, 8 }, { 2, 4, 6 } };
+        int[][] winningSequences = { 
+            { 0, 1, 2 }, 
+            { 3, 4, 5 }, 
+            { 6, 7, 8 }, 
+            { 0, 3, 6 }, 
+            { 1, 4, 7 }, 
+            { 2, 5, 8 },
+            { 0, 4, 8 }, 
+            { 2, 4, 6 } 
+        };
+        
         for (int[] sequence : winningSequences) {
             if (this.ticTacToe[sequence[0]].equals(symbol) && this.ticTacToe[sequence[1]].equals(symbol)
                     && this.ticTacToe[sequence[2]].equals(symbol)) {
