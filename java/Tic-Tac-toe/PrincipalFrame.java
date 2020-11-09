924984a8cc1cb35dfd4f6da816ca6c366a4e8e9c
AndrewsVieira <andrews.a.vieira@gmail.com>
2020-11-09 10:58:07 -0300

    fim do jogo, reboot do jogo e close do jogo

--- java/Tic-Tac-toe/PrincipalFrame.java
+++ java/Tic-Tac-toe/PrincipalFrame.java
@@ -1,5 +1,6 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import java.awt.Color;
 import java.awt.GridLayout;
@@ -11,9 +12,11 @@ public class PrincipalFrame extends JFrame {
 
     private JButton[] btns = new JButton[NUM_BTN];
     private int sizeArrayBtns = btns.length;
+    private String title;
 
     public PrincipalFrame(String title) {
         super(title);
+        this.title = title;
 
         this.setSize(640, 480);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,7 +24,7 @@ public class PrincipalFrame extends JFrame {
         init();
     }
 
-    public void init() {
+    private void init() {
         ActionEventHandler handler = new ActionEventHandler();
 
         setLayout(new GridLayout(3, 3, 0, 0));
@@ -31,7 +34,6 @@ public class PrincipalFrame extends JFrame {
             add(btns[i]);
             btns[i].addActionListener(handler);
         }
-        
 
     }
 
@@ -64,16 +66,28 @@ public class PrincipalFrame extends JFrame {
             if (position > -1) {
                 game.setTicTacToe(symbolTurnIndex, position);
                 System.out.println(game.toString());
-                
+
                 if (game.checkWinningSequences(symbolTurnIndex) || game.checkTie()) {
                     // parar os eventos de click's quando terminar o jogo
                     for (int i = 0; i < sizeArrayBtns; i++) {
                         btns[i].removeActionListener(this);
                     }
+                    // caixa de diálogo
+                    int answer = JOptionPane.showConfirmDialog(null,
+                            "The symbols '" + symbolTurnIndex + "' wins!\nWould you like to play again?");
+                    if (answer == JOptionPane.OK_OPTION) {
+                        setVisible(false);
+                        // reiniciar o jogo
+                        JFrame frame = new PrincipalFrame("Tic-Tac-Toe");
+                        frame.setVisible(true);
+                    } else if (answer == JOptionPane.NO_OPTION) {
+                        setVisible(false);
+                    }
+
                     System.out.println("Game-over");
                 }
             }
-            
+
             symbol.change();
         }
     }
--- java/Tic-Tac-toe/TicTacToe.java
+++ java/Tic-Tac-toe/TicTacToe.java
@@ -6,8 +6,6 @@ public class TicTacToe {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // TicTacToe ticTacToe = new TicTacToe();
-
         JFrame frame = new PrincipalFrame("Tic-Tac-Toe");
         frame.setVisible(true);
     }
