98c6c4a4d17a75facc6b95195f870d166f26fbc7
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-02-24 21:34:19 -0300

    primeiro exercicio em c

---
+++ c/fixacao4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int *p;
+
+    a = 10;
+    p = (int *) malloc(sizeof(int));
+    *p = 100;
+
+    printf("A soma de %d e %d é %d\n", a, *p, a + *p);
+}
