dbef0975871739351137694ef07c5bd6e9475f8a
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-03-09 11:24:52 -0300

    estruturas heterogeneas

--- c/fixacao7.c
+++ c/fixacao7.c
@@ -11,8 +11,42 @@ apresentação. [fixacao7.c]
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 int main() {
+    typedef struct ListaCompras_{
+        char produto[10];
+        float valorProduto;
+    } ListaCompra;
+
+    typedef struct Compra_{
+        char nome[15];
+        float valor;
+        ListaCompra lista[10];        
+    } Compra;
+
+    int i;
+    int c;
+
+    Compra compra;
+    strcpy(compra.nome, "MERCADO TONINHO");
+    strcpy(compra.lista[0].produto, "ÁGUA");
+    compra.lista[0].valorProduto = 1.80;
+
+    strcpy(compra.lista[1].produto, "COCA-COLA");
+    compra.lista[1].valorProduto = 3.50;
+
+    printf("----%s----\n", compra.nome);
+printf("%s %-10s\t%s\n", "Item", "Produto", "Valor");
+    printf("-----------------------\n");
     
+    compra.valor = 0.0;
+    for (i = 0; i < 2; i++) {
+        compra.valor += compra.lista[i].valorProduto;
+
+        printf("%-4d%-10s\t%.2f\n", i+1, compra.lista[i].produto, compra.lista[i].valorProduto);
+    }
+
+    printf("-----------------------\n");
+    printf("%-10s\t%.2f\n", "TOTAL", compra.valor);
 }
